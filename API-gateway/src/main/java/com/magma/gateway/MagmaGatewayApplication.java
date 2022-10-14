50db9854cfe59c22462ef520ba3294cf7a399d7d
Parameswaran Sajeenthiran <77486691+ParameswaranSajeenthiran@users.noreply.github.com>
2022-10-14 14:56:59 +0530

    Add files via upload

---
+++ API-gateway/src/main/java/com/magma/gateway/MagmaGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.magma.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+public class MagmaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagmaGatewayApplication.class, args);
+	}
+
+}
---
+++ API-gateway/src/test/java/com/magma/gateway/MagmaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magma.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagmaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
